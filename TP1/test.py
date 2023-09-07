f5d17e79c5a80c23d75a50d497efc91e89d50deb
Duval <lyam.duval@univ-rouen.fr>
2023-09-07 16:39:38 +0200

    ajout de test dans test.py

--- TP1/test.py
+++ TP1/test.py
@@ -6,5 +6,10 @@ def test_1():
 	assert f.puissance(2,2) == 4
 
 def test_2():
-        assert f.puissance(2,3) ==-8
-        assert f.puissance(2,2) ==-4
+        assert f.puissance(2,4) ==16
+        assert f.puissance(8,0) ==1
+
+def test_3():
+	assert f.puissance(-1,3) == -1
+        assert f.puissance(-2,2) == 4
+	assert f.puissance(0,1) == 0
