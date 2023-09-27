7d192de4f6a1ff18a1d73088d5f01df9cf355f05
Duval <lyam.duval@univ-rouen.fr>
2023-09-27 15:11:31 +0200

    ajout de la boucle for dans fonction.py

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -3,7 +3,11 @@ def puissance (a,b):
 		raise TypeError ("only integers are allowed")
 	if not type(b) is int:
                 raise TypeError ("only integers are allowed")
-	puis=a**b
+	i=0
+	puis=a
+	for i in range (1,b): 
+		puis=puis*a
+		i=i+1
 	return puis
 	print ("voici la valeur du 1er nombre élever à la puissance de la 2eme valeur: ",puis)
 
