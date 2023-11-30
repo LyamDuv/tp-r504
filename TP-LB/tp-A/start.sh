b358c154a8ae1c8d58cd021f0af8610bcdf75170
LyamDuv <lyam.duval@univ-rouen.fr>
2023-11-30 13:39:08 +0000

    fin de première partie de tp

--- TP-LB/tp-A/start.sh
+++ TP-LB/tp-A/start.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-docker build -t im-nginx-lb Dockerfile .
+docker build -t im-nginx-lb -f Dockerfile .
 
 mkdir -p shared1
 mkdir -p shared2
 
-echo "<h1>Hello1</h1>" > /shared1/index.html
-echo "<h1>Hello2</h1>" > /shared2/index.html
+echo "<h1>Hello 1</h1>" > shared1/index.html
+echo "<h1>Hello 2</h1>" > shared2/index.html
 
-docker run -d -p 81:80 --name nginx1 im-nginx-lb
-dicker run -d -p 82:80 --name nginx2 im-nginx-lb
+docker run -d \
+	--name nginx1 -p 81:80 -v "$(pwd)/shared1:/usr/share/nginx/html" nginx
+docker run -d \
+	--name nginx2 -p 82:80 -v "$(pwd)/shared2:/usr/share/nginx/html" nginx
+
+docker run -d -p 83:80 im-nginx-lb
+
+hello1=0
+hello2=0
+
+for ((i = 1; i <= 500; i++)); 
+do
+    b="<h1>Hello 1</h1>"
+    c="<h1>Hello 2</h1>"
+    a=$(curl -s http://localhost:83/)  # Utilisation de $(...) pour capturer la sortie de la commande curl
+
+    if [ "$a" == "$b" ]; then
+        ((hello1++))
+    fi
+
+    if [ "$a" == "$c" ]; then
+        ((hello2++))
+    fi
+done
+
+echo "Nombre de pages avec hello 1 : $hello1"
+echo "Nombre de pages avec hello 2 : $hello2"
