fd04066024616592a56f59f929c53b476d81c301
user <user@user>
2024-01-22 04:31:56 -0600

    fin de tp

---
+++ TP9/Clienthttp.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.net.*;
+
+public class ClientTCP3 {
+    public static void main(String[] args) {
+        try {
+            
+        } catch (Exception ex) {
+            System.out.println("Erreur !");
+            ex.printStackTrace();
+        }
+    }
+}
